d356424d83a876265ead1b08b050bb4fd88e2d58
TONI <TONI@TONI-PC>
2017-01-12 21:57:58 +0530

    Initial Comit

---
+++ webdriverframework/src/main/java/com/toni/library/utils/DriverFactory.java
@@ -0,0 +1,12 @@
+package com.toni.library.utils;
+
+import java.util.Properties;
+
+public class DriverFactory {
+	
+	private static String chromeDriver;
+	private static String fireBug;
+	private static String firefoxDriver;
+	private static Properties p = null;
+
+}
